90f2105c3d31758bb74655810f2cb2aa22862fac
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-02-24 13:05:29 +0100

    Create threadalert.lang.php

---
+++ inc/languages/deutsch_du/threadalert.lang.php
@@ -0,0 +1,6 @@
+<?php
+$l['threadalert_editpost'] = "Alert-Option:";
+$l['threadalert_editpost_desc'] = "Bezieht sich auf das komplette Thema. Option kann für einzelne (neue) Beiträge individuell ein bzw. ausgestellt werden.";
+$l['threadalert_option'] = "<b>Alert verschicken:</b> An alle Accounts ein Alert schicken.";
+$l['threadalert_alert'] = "<b>Wichtig!</b> Im Thema \"{2}\" wurde ein neuer Beitrag gepostet.";
+$l['myalerts_setting_threadalert_alert'] = "Benachrichtigungen erhalten von neuen Beiträgen in besonderen Themen vom Team?";
